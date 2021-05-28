7f2feab472b4eb61c635b4a612cac2df47c51872
Lucas Riera <lucasezequielriera@hotmail.com>
2021-05-28 15:48:39 -0300

    doing index

---
+++ js/index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/////----- Main -----/////
+
+// Consts //
+const body = document.querySelector('body');
+const modalWindow = document.querySelector('#modal');
+const search = document.querySelector('#search');
+const formSearch = document.querySelector('#formSearch');
+const submitButton = document.querySelector('#submit-button');
+
+// Listeners //
+search.addEventListener("focus", showModal);
+search.addEventListener("blur", hideModal);
+
+// Show Modal Function //
+function showModal() {
+    modalWindow.style.zIndex = 9999;
+    formSearch.style.zIndex = 10000;
+    modalWindow.style.backgroundColor = 'rgba(0,0,0,0.7)';
+    submitButton.classList.remove('btn-outline-danger');
+    submitButton.classList.add('btn-danger');
+}
+
+// Hide Modal Function //
+function hideModal() {
+    modalWindow.style.zIndex = -1;
+    modalWindow.style.backgroundColor = 'transparent';
+    submitButton.classList.remove('btn-danger');
+    submitButton.classList.add('btn-outline-danger');
+}
+
+/////----- / Main -----/////
